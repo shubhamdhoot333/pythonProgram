e9fecf6f64bee922b70353d5e3fa045a6883182b
shubhamdhoot333 <45518300+shubhamdhoot333@users.noreply.github.com>
2019-02-26 16:54:27 +0530

    Add files via upload

---
+++ largeNumberIn3.py
@@ -0,0 +1,13 @@
+a=int(input("enter number"));
+b=int(input("enter number"));
+c=int(input("enter number"));
+if(a>b):
+    if(a>c):
+        print("a is big")
+    else:
+        print("c is big")
+else:
+    if(b>c):
+        print("b is big")
+    else:
+        print("c is big")
